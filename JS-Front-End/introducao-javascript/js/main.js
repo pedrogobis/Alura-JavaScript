04c8688cc22473db6f620071fd6e22ec513e48b8
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-05-20 16:29:13 -0300

    Update .02
    - Melhorado a interface.
    - Automatizado todos os itens da tabela

--- JS-Front-End/introducao-javascript/js/main.js
+++ JS-Front-End/introducao-javascript/js/main.js
@@ -1,34 +1,55 @@
 let titulo = document.querySelector('h1');
 titulo.textContent = 'ApariNutri';
 
-var paciente = document.querySelector('#primeriopass');
-var tdpeso = paciente.querySelector('.info-peso');
-var peso = tdpeso.textContent;
-var tdaltura = paciente.querySelector('.info-altura')
-var altura = tdaltura.textContent;
-var tdimc = paciente.querySelector('.info-imc') 
-var pesoValido = true;
-var alturaValida = true;
+// Variaveis do codigo
+var pacientes = document.querySelectorAll('.paciente');
 
+for(var i= 0; i< pacientes.length;i++){
+    // corrigiu o codigo feito para 1 item apenas
+    // criou a variavel paciente que vai ser o indice no array pacientes
+    var paciente = pacientes[i]
+    
+    var tdpeso = paciente.querySelector('.info-peso');
+    var peso = tdpeso.textContent;
+    var tdaltura = paciente.querySelector('.info-altura')
+    var altura = tdaltura.textContent;
+    var tdimc = paciente.querySelector('.info-imc') 
+    var pesoValido = true;
+    var alturaValida = true;
 
-if(peso <= 0 || peso >= 1000){
-    console.log('peso invalido fera')
-    pesoValido = false;
-    tdimc.textContent = 'Peso invalido'
+    // Verificação dos dados.
+    if(peso <= 0 || peso >= 400){
+        console.log('peso invalido fera')
+        pesoValido = false;
+        tdimc.textContent = 'Peso invalido'
+        paciente.classList.add('paciente-invalido')
+        //Não é correto alterar elementos especificos dentro do js, js devemos apenas alterar as classes, então o correto é adicionar uma classe dentro do css, e então no js alterar a classe.
+        // Tudo que é estilo fica no css
+        // tudo que é interação fica no JS.
+        //paciente.style.backgroundColor = 'red'; // styles com duas palavras, usar o padrão camelcase para funcionar.
+    }
+    if(altura <= 0 || altura >= 3.00){
+        console.log('altura invalida fera')
+        alturaValida = false;
+        tdimc.textContent = 'Altura invalida'
+        paciente.classList.add('paciente-invalido')
+        //paciente.style.backgroundColor = 'red';
+    }
+    
+    if(alturaValida && pesoValido){
+        var imc = peso /  (altura * altura);
+        tdimc.textContent = imc.toFixed(2); // toFixed(2) arruma a casa decimal a ser mostrada
+    
+    }else{
+        tdimc.textContent = 'Altura e/ou peso invalidos.'
+    }
 }
-if(altura <= 0 || altura >= 3.00){
-    console.log('altura invalida fera')
-    alturaValida = false;
-    tdimc.textContent = 'Altura invalida'
-}
 
-if(alturaValida && pesoValido){
-    var imc = peso /  (altura * altura);
-    tdimc.textContent = imc;
 
-}else{
-    tdimc.textContent = 'Altura e/ou peso invalidos.'
-}
+
+
+
+
 
 
 
