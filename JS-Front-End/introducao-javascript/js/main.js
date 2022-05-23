3c271ee52a9f9689cc63c4341ed3dde1349ecce8
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-05-23 14:12:53 -0300

    Update
    -pessoas sendo inseridas na tabela

--- JS-Front-End/introducao-javascript/js/main.js
+++ JS-Front-End/introducao-javascript/js/main.js
@@ -1,8 +1,8 @@
+// variaveis
 let titulo = document.querySelector('h1');
-titulo.textContent = 'ApariNutri';
+let botaoAdicionar = document.querySelector('#adicionar-paciente');
+let pacientes = document.querySelectorAll('.paciente');
 
-// Variaveis do codigo
-var pacientes = document.querySelectorAll('.paciente');
 
 for(var i= 0; i< pacientes.length;i++){
     // corrigiu o codigo feito para 1 item apenas
@@ -43,19 +43,55 @@ for(var i= 0; i< pacientes.length;i++){
     }else{
         tdimc.textContent = 'Altura e/ou peso invalidos.'
     }
-}
+};
+
+/*
+titulo.addEventListener("click", function (){
+    alert('olám voce clicou aqui')
+}) // atrelando eventos a funcoes
+*/
+
+botaoAdicionar.addEventListener('click',function (event){
+    event.preventDefault();// evento para nao deixar o botao do form recarregar a pagina, para isso precisou colocar um parametro na funcao sem nome
+    
+    // Variaveis 
+
+    let formulario = document.querySelector("#form-adiciona")// pelo form da pra pegar todos os dados dele direto, sem ter  que criar uma variavel para cada
+    // console.log(formulario.altura.value);// pegando os dados do form usando o . direto 
+   
+    let nome = formulario.nome.value;
+    let peso = formulario.peso.value;
+    let altura = formulario.altura.value;
+    let gordura = formulario.gordura.value;
 
 
+    // criando elementos
+    let pacientetr = document.createElement('tr')// criou a linha
+    
+    // criou os as colunas
+    let nometd = document.createElement('td')
+    let pesotd = document.createElement('td')
+    let alturatd = document.createElement('td')
+    let gorduratd = document.createElement('td')
+    
+    // dando o valor pra cada td
+    nometd.textContent = nome;
+    pesotd.textContent = peso;
+    alturatd.textContent = altura;
+    gorduratd.textContent = gordura;
+
+    // adicionando os dados como filhos
+    pacientetr.appendChild(nometd);
+    pacientetr.appendChild(pesotd);
+    pacientetr.appendChild(alturatd);
+    pacientetr.appendChild(gorduratd);
+
+    // selecionando a tabela que vai receber os dados
+    let tabela = document.querySelector("#tabela-pacientes")
+
+    tabela.appendChild(pacientetr);
 
 
+});
 
 
-
-
-
-
-console.log(paciente)
-console.log(tdpeso)
-console.log(peso) 
-console.log(altura) 
-console.log(imc) 
