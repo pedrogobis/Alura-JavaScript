05a4f72a22595d11a1761880aada3886097b8259
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-05-25 09:37:43 -0300

    Create ternario.js

---
+++ JS-Front-End/JS Basico/ternario.js
@@ -0,0 +1,13 @@
+//ternario funciona como um if
+
+const idadeMinima = 18;
+let idadeCliente = 15;
+
+/*
+if(idadeCliente >= idadeMinima){
+    console.log('IH JA PODE BEBER FI')
+} console.log('HUUUM QUE BEBE? PO NÃO, IXQUECE TATA É FODA')
+*/
+
+// comparação simples. e com resposta simples.
+console.log(idadeCliente >= idadeMinima? "ih ja pode beber": "esquece, tata é foda")
