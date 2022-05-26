b2ff0f5440a10936c1324387fb1456a4fa809ee3
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-05-26 10:29:23 -0300

    Aula

---
+++ JS-Front-End/JS Basico/arrays.js
@@ -0,0 +1,32 @@
+//Medias 10 65 8 7 5
+let n1 = 10;
+let n2 = 6.5;
+let n3 = 8;
+let n4 = 7.5;
+
+
+
+// vamos fazer com array agora
+
+let notas =  [ 10, 6.5, 8]
+let media  = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+console.log(media)
+
+
+// inserindo nota no array
+//notas.pop(2)// remove o valor no indice 2
+console.log(notas)
+notas.push(7.5) // adicionou na tabela, na ultima posição;
+console.log(notas)
+
+
+/*
+montagem do for e do for each estão montadas corretamente, problema é que não to lembrando do que fazer dentro do loop;
+for(let i = 0; i > notas.length; i++){
+    let nota = notas[i];
+    nota += nota[i]
+    
+}
+ carai to peidando no foreach
+co
+*/
