960d1887f2090916a8dfff6cbe3d8cedf49b0dd6
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-06-09 16:54:39 -0300

    Consegui importar a bendita
    - Usar o terminal, nao apenas o output do visual, da erro quando não tem

---
+++ JS-Front-End/JS Basico/algoritimosParteDois/arquivos-base/aula-4/playground.js
@@ -0,0 +1,8 @@
+
+var numbers = [4, 2, 5, 1, 3];
+numbers.sort(function(a, b) {
+  return a - b;
+});
+console.log(numbers);
+// Saída 
+//[1, 2, 3, 4, 5]
---
+++ JS-Front-End/JS Basico/criandoBibliotecas/index.js
@@ -0,0 +1,16 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
+
+console.log(chalk.blue('vamos começar!'));
+
+const paragrafo = 'Texto retornado por uma função';
+
+function texto(string) {
+  return string;
+}
+
+console.log(texto(paragrafo));
+
+
+/// pra funcionar precisa rodar no terminal e não no output, da erro.
