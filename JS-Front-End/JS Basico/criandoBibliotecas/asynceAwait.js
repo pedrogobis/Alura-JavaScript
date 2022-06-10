c03bc29c4b09e5c30ca00852bc9e2a4435f29e17
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-06-10 17:05:26 -0300

    Update asynceAwait.js

--- JS-Front-End/JS Basico/criandoBibliotecas/asynceAwait.js
+++ JS-Front-End/JS Basico/criandoBibliotecas/asynceAwait.js
@@ -3,6 +3,27 @@ import chalk from 'chalk';
 import fs from 'fs'; // biblioteca para ler arquivos em js.
 const caminho = './arquivo/texto1.md' // caminho do arquivo.
 
+
+
+
+// regex \[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\) \\ retorna dois grupos, um grupo só com o nome e um grupo só com o link.
+
+function extraiLinks(texto){
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm; //
+    const arrayResult = [];
+    //const linksExtraidos = texto.match(regex); // match é um metodo de string
+    //const linksExtraidos = regex.exec(texto)// passamos o texto que vai ser executado
+    let temp;
+    while((temp = regex.exec(texto)) !== null){
+        arrayResult.push({[temp[1]]: temp[2]}) // chave do objeto precisa estar entre colchetes
+    }
+    //console.log(arrayResult) // teste deu certo
+    return arrayResult;
+}
+
+//extraiLinks(texto1)// testado e funcionando
+
+
 /// pra funcionar precisa rodar no terminal e não no output, da erro.
 
 function trataErro(erro){
@@ -10,11 +31,12 @@ function trataErro(erro){
   throw new Error(chalk.red(erro.code, 'arquivo não encontrado.'));
 }
 
+
 async function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
     try{
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)// pegando o texto
-        console.log(chalk.greenBright(texto)) // se fosse sincrono era só deixar assim
+        console.log(extraiLinks(texto)) // se fosse sincrono era só deixar assim
 
     }catch(erro){
         trataErro(erro);
