472200d360a083824118f5faac0486f133791876
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-06-20 23:29:50 -0300

    feito

--- JS-Front-End/JS Basico/criandoBibliotecas/cli.js
+++ JS-Front-End/JS Basico/criandoBibliotecas/cli.js
@@ -7,7 +7,7 @@ async function processaTexto(caminhoDoArquivo){
     try{
         const resultado = await pegaArquivo(caminhoDoArquivo[2]);
     
-        console.log(chalk.yellow('lista de links'), resultado)
+        return console.log(chalk.yellow('lista de links'), resultado)
     }
     catch(erro){
         return trataErro(erro)
