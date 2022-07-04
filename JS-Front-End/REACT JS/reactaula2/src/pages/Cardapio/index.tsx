77237c8aae966906ff20d686069daeac2208200c
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-07-04 15:20:39 -0300

    Imagem
    -Segunda forma e até mais limpa para importar um svg/Imagem.
    
    Importamos como um reactComponent, assim quando colocarmos no html importamos como se fosse uma tag

--- JS-Front-End/REACT JS/reactaula2/src/pages/Cardapio/index.tsx
+++ JS-Front-End/REACT JS/reactaula2/src/pages/Cardapio/index.tsx
@@ -1,11 +1,11 @@
 import styles from './Cardapio.module.scss'// import do modulo css.
-import logo from '../../assets/logo.svg' // precisamos importar a img como um objeto, aparente tem dois jeitos, e do outro modo serve pra manter como svg
+import {ReactComponent as Logo} from '../../assets/logo.svg' //Outra forma de importar, de uma maneira mais facil, manteve um svg como svg, e ficou até mais limpo o codigo
 
 export default function Cardapio(){
     return(
         <main>
             <nav className={styles.menu}>
-                <img src={logo} alt="Aluroni" />
+                <Logo></Logo>
             </nav>
             
         </main>
