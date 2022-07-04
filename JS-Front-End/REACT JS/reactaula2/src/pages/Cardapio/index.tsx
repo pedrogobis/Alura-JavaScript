b7230379fbf2a9fb894e10e867fd586638cf9971
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-07-04 15:18:42 -0300

    Importando uma img
    - Importamos uma imagem e fizemos funcionar do jeito padrão.
    
    Importamos ela como um objeto no javascript, e funcionnou

--- JS-Front-End/REACT JS/reactaula2/src/pages/Cardapio/index.tsx
+++ JS-Front-End/REACT JS/reactaula2/src/pages/Cardapio/index.tsx
@@ -1,10 +1,11 @@
 import styles from './Cardapio.module.scss'// import do modulo css.
+import logo from '../../assets/logo.svg' // precisamos importar a img como um objeto, aparente tem dois jeitos, e do outro modo serve pra manter como svg
 
 export default function Cardapio(){
     return(
         <main>
-            <nav className="menu">
-
+            <nav className={styles.menu}>
+                <img src={logo} alt="Aluroni" />
             </nav>
             
         </main>
