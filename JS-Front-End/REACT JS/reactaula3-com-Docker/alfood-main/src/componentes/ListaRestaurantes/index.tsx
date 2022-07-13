024f08dfcd10b2e7786dd7d8d13c82c11b03a2bf
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-07-13 16:17:31 -0300

    Update
    - Editando o nome do restaurante
    - Deletando o nome do restaurante
    - fazendo a busca de um restaurante

--- JS-Front-End/REACT JS/reactaula3-com-Docker/alfood-main/src/componentes/ListaRestaurantes/index.tsx
+++ JS-Front-End/REACT JS/reactaula3-com-Docker/alfood-main/src/componentes/ListaRestaurantes/index.tsx
@@ -1,43 +1,73 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
 import { IPaginacao } from '../../interfaces/IPaginacao';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 
+// esses são os possíveis parâmetros que podemos enviar para a API
+interface IParametrosBusca {
+  ordering?: string
+  search?: string
+}
+
+  
+
+
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [proximaPagina, setProximaPagina] = useState('')
+  const [paginaAnterior, setPaginaAnterior] = useState('')
 
-  useEffect(() => {
-    // OBTEM RESTAURANTES
-    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
+  const [busca, setBusca] = useState('')
+
+
+  const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
+
+    axios.get<IPaginacao<IRestaurante>>(url, opcoes)
       .then(resposta => {
         setRestaurantes(resposta.data.results)
         setProximaPagina(resposta.data.next)
-      })
-      .catch(erro => {
-        console.log(erro)
-      })
-  }, [])
-
-  const verMais = () => {
-    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
-    .then(resposta => {
-        setRestaurantes([...restaurantes, ...resposta.data.results])
-        setProximaPagina(resposta.data.next)
+        setPaginaAnterior(resposta.data.previous)
       })
       .catch(erro => {
         console.log(erro)
       })
   }
 
+  const buscar = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+    const opcoes = {
+      params: {
+
+      } as IParametrosBusca
+    }
+    if (busca) {
+      opcoes.params.search = busca
+    }
+    carregarDados('http://localhost:8000/api/v1/restaurantes/', opcoes)
+  }
+
+useEffect(() => {
+    // obter restaurantes
+    carregarDados('http://localhost:8000/api/v1/restaurantes/')
+  }, [])
+
+
+
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    <form onSubmit={buscar}>
+      <input type="text" value={busca} onChange={evento => setBusca(evento.target.value)} />
+      <button type='submit'>buscar</button>
+    </form>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
-    {proximaPagina && <button onClick={verMais}>
-      Ver mais
+    {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}>
+      Página Anterior
+    </button>}
+    {<button onClick={() => carregarDados(proximaPagina)} disabled={!proximaPagina}>
+      Próxima página
     </button>}
   </section>)
 }
--- JS-Front-End/REACT JS/reactaula3-com-Docker/alfood-main/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ JS-Front-End/REACT JS/reactaula3-com-Docker/alfood-main/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,6 +1,7 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 import IRestaurante from "../../../interfaces/IRestaurante"
 
 const AdministracaoRestaurantes = () => {
@@ -11,6 +12,18 @@ const AdministracaoRestaurantes = () => {
             .then(resposta => setRestaurantes(resposta.data))
     }, [])
 
+    const excluir = (restauramteASerExcluido: IRestaurante) => {
+        axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauramteASerExcluido.id}/`)
+        .then(() => {
+            alert(`Restaurante ${restauramteASerExcluido.nome} foi excluido com sucesso!`)
+            const listaRestaurante = restaurantes.filter(restaurante => restaurante.id !== restauramteASerExcluido.id)
+            
+            setRestaurantes([...listaRestaurante])
+
+            
+        })
+    }
+
     return (
         <TableContainer component={Paper}>
             <Table>
@@ -19,12 +32,26 @@ const AdministracaoRestaurantes = () => {
                         <TableCell>
                             Nome
                         </TableCell>
+                        <TableCell>
+                            Editar
+                        </TableCell>
+                        <TableCell>
+                            Excluir
+                        </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
                         <TableCell>
                             {restaurante.nome}
+                        </TableCell>
+                        <TableCell>
+                            [ <Link to={`/admin/restaurantes/${restaurante.id}`}>Editar</Link> ]
+                        </TableCell>
+                        <TableCell>
+                            <Button variant="outlined" color="error" onClick={() => excluir(restaurante)}>
+                                Excluir
+                            </Button>
                         </TableCell>
                     </TableRow>)}
                 </TableBody>
